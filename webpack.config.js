4dcd575c2c79af2911379b6ff2953e5fe1a0e9a5
17aim17 <17aim17@gmail.com>
2019-02-24 19:03:08 +0530

    webpack configured for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,61 @@
 //entry point and final otput file
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+module.exports = env => {
+  console.log('env', env);
+  const isProduction = env === 'production';
 
-          // ,
-          // {
-          //   loader: 'postcss-loader',
-          //   options: {
-          //     ident: 'postcss',
-          //     plugins: () => [require('autoprefixer')]
-          //   }
-          // }
-        ]
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8000, // Convert images < 8kb to base64 strings
-              name: 'images/[hash]-[name].[ext]'
+  return {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [require('autoprefixer')]
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'build')
-  }
+          ]
+        },
+        {
+          test: /\.(png|jp(e*)g|svg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'build')
+    }
+  };
 };
